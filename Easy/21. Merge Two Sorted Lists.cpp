8d6ac8444de94250c97dc08472ab4337c063b969
JiteshGupta17 <54665900+JiteshGupta17@users.noreply.github.com>
2020-02-10 18:12:54 +0530

    Linked List

--- Easy/21. Merge Two Sorted Lists.cpp
+++ Easy/21. Merge Two Sorted Lists.cpp
@@ -29,31 +29,22 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     {
         if (l1->val <= l2->val)
         {
-            cur->next = new ListNode(l1->val);
+            cur->next = l1;
             l1 = l1->next;
         }
         else {
-            cur->next = new ListNode(l2->val);
+            cur->next = l2;
             l2 = l2->next;
         }
 
         cur = cur->next;
     }
 
-    while (l1)
-    {
-        cur->next = new ListNode(l1->val);
-        cur = cur->next;
-        l1 = l1->next;
-    }
+    if (l1)
+        cur->next = l1;
 
-    while (l2)
-    {
-        cur->next = new ListNode(l2->val);
-        cur = cur->next;
-        l2 = l2->next;
-    }
+    if (l2)
+        cur->next = l2;
 
     return dummyHead->next;
 }
-
