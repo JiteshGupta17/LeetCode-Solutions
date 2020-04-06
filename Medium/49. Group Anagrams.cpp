0aeaa1827ef9455bc0ae30d396455e7075cc4edd
JiteshGupta17 <54665900+JiteshGupta17@users.noreply.github.com>
2020-04-06 13:46:13 +0530

    HashMap, Sort

--- Medium/49. Group Anagrams.cpp
+++ Medium/49. Group Anagrams.cpp
@@ -1,27 +1,60 @@
+// Method 1 - Using our own sort function as only lowercase letters
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        // Sort each string which acts as key in the map
-        unordered_map<string,vector<string>> m;
-        
-        vector<vector<string>> ans;
-        
-        for(string s : strs)
-        {
-            string cur = s;
-            sort(s.begin(),s.end());
-            
-            m[s].push_back(cur);
-            
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+
+            mp[mysort(s)].push_back(s);
+        }
+
+        vector<vector<string>> anagrams;
+
+        for (auto p : mp) {
+            anagrams.push_back(p.second);
+        }
+
+        return anagrams;
+    }
+private:
+    string mysort(string s) {
+        int counter[26] = {0};
+
+        for (char c : s) {
+            counter[c - 'a']++;
         }
         
-        for(auto ele : m)
-        {
-            ans.push_back(ele.second);
+        string t;
+
+        for (int c = 0; c < 26; c++) {
+            t += string(counter[c], c + 'a');
         }
-        
-        return ans;
-        
+        return t;
     }
 };
+// ==========================================================================================
+
+// Method 2 - Using inbuilt sort
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> m;
+
+        for (string& s : strs) {
+            string t = s;
+            sort(t.begin(), t.end());
+            m[t].push_back(s);
+        }
+
+        for (auto& e : m) {
+            res.push_back(e.second);
+        }
+
+        return res;
+    }
+};
+
+// ===========================================================================================
+
