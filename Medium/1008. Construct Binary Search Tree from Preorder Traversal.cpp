a76f8753ff5d4ed225f9e9132bef0a030d301914
JiteshGupta17 <54665900+JiteshGupta17@users.noreply.github.com>
2020-04-20 21:43:39 +0530

    Binary Search Tree

--- Medium/1008. Construct Binary Search Tree from Preorder Traversal.cpp
+++ Medium/1008. Construct Binary Search Tree from Preorder Traversal.cpp
@@ -1,44 +1,73 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// Recursive
 class Solution {
 public:
-    
-    TreeNode* buildTree(TreeNode* node,int x)
+
+    TreeNode* buildTree(TreeNode* node, int x)
     {
-        if(node==NULL)
+        if (node == NULL)
         {
-           return new TreeNode(x); 
+            return new TreeNode(x);
         }
-        
-        if(node->val > x)
+
+        if (node->val > x)
         {
-            node->left = buildTree(node->left,x);
+            node->left = buildTree(node->left, x);
         }
         else
-            node->right = buildTree(node->right,x);
-        
+            node->right = buildTree(node->right, x);
+
         return node;
-        
-        
+
     }
-    
+
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        if(preorder.empty()) return NULL;
-        
+        if (preorder.empty()) return NULL;
+
         TreeNode* root = new TreeNode(preorder[0]);
-        
-        for(int i = 1;i<preorder.size();i++)
+
+        for (int i = 1; i < preorder.size(); i++)
         {
-            buildTree(root,preorder[i]);
+            buildTree(root, preorder[i]);
         }
-        
+
+        return root;
+    }
+};
+
+// Iterative
+class Solution {
+public:
+
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        if (preorder.empty()) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[0]);
+
+        for (int i = 1; i < preorder.size(); i++)
+        {
+            int x = preorder[i];
+            TreeNode* node = root;
+
+            while (true)
+            {
+                if (node->val > x)
+                {
+                    if (!node->left) {
+                        node->left = new TreeNode(x);
+                        break;
+                    }
+                    else node = node->left;
+                }
+                else {
+                    if (!node->right) {
+                        node->right = new TreeNode(x);
+                        break;
+                    }
+                    else node = node->right;
+                }
+            }
+        }
+
         return root;
     }
 };
